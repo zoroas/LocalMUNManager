43e5f880ffd0e4634e637a889273e968cc1f6414
Marc0Costa <marco.bc.costa@caislisbon.org>
2021-09-06 09:15:33 +0100

    Changed AssemblyInfo

--- LocalMUNManager/Properties/AssemblyInfo.cs
+++ LocalMUNManager/Properties/AssemblyInfo.cs
@@ -50,8 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.52")]
-[assembly: AssemblyFileVersion("1.0.0.52")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: Guid("4CBE9A26-DAF8-4F96-8113-B2090E46DDB3")]
 
